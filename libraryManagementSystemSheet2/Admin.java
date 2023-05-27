dd5a7ae0a6fad021c726bdf7ea6a7571a12e5135
Jrana1 <jewelbutex12@gmail.com>
2023-05-27 13:47:45 +0200

    admin class has been added

---
+++ libraryManagementSystemSheet2/Admin.java
@@ -0,0 +1,8 @@
+public class Admin {
+    private Catalogue catalogue;
+    private Lender lender;
+    private Title title;
+    private Rental rental;
+    private UserManagement userManagement;
+
+}
